b0fbb72e6eb48f75e4297175bed1265c1f03a2c6
Olivier <olivier.pire@outlook.fr>
2022-07-08 09:33:30 +0200

    Maj responsive more-details + placement responsive des logos accueil + Ajout page 404

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
-import { Routes, Route, HashRouter } from "react-router-dom";
+import { Routes, Route, HashRouter, BrowserRouter } from "react-router-dom";
 import About from "./pages/About";
 import Home from "./pages/Home";
 import Portfolio from "./pages/Portfolio";
-import './styles/index.scss'
+import "./styles/index.scss";
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
+import { Audio } from "react-loader-spinner";
 
 function App() {
-  return (
-    <HashRouter>
-      <Routes>
-        <Route path="/" exact element={<Home />} />
-        <Route path="/portfolio" exact element={<Portfolio />} />
-        <Route path="/about" exact element={<About />} />
-      </Routes>
-    </HashRouter>
-  );
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" exact element={<Home />} />
+				<Route path="/portfolio" exact element={<Portfolio />} />
+				<Route path="/about" exact element={<About />} />
+			</Routes>
+		</BrowserRouter>
+	);
 }
 
 export default App;
